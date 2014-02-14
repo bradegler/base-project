02c90d1e70ca7cb3cee80e439062dfc58d04874c
Brad Egler <begler@gmail.com>
2014-02-14 16:11:13 -0500

    added some notes to the readme, minor gulfile changes

--- README.md
+++ README.md
@@ -11,3 +11,22 @@ Mainly just playing with different libraries
 * [Bourbon](http://bourbon.io/)
 * [MongoDB](http://www.mongodb.org/)
 * [Mongoose](http://mongoosejs.com/)
+
+## Notes
+If you run out of allowable file watches with an error like below
+
+    [gulp] Errored 'default' in 12 ms watch ENOSPC
+
+    ./node_modules/gulp/node_modules/orchestrator/index.js:153
+            throw err;
+                  ^
+    Error: watch ENOSPC
+        at errnoException (fs.js:1019:11)
+        at FSWatcher.start (fs.js:1051:11)
+        at Object.fs.watch (fs.js:1076:11)
+
+You can fix this by increasing the max user watches like below
+
+    sudo sysctl -w fs.inotify.max_user_watches=524288
+    sudo sysctl -p
+
--- gulpfile.js
+++ gulpfile.js
@@ -9,66 +9,66 @@ var sass = require('gulp-sass');
 var bourbon = require('node-bourbon').includePaths;
 var watch = require('gulp-watch');
 
-gutil.log('Environment', gutil.colors.blue(gulp.env.production ? 'Production' : 'Development'));
+gutil.log('Environment', gutil.colors.blue(gutil.env.production ? 'Production' : 'Development'));
 
 gulp.task('scripts', function() {
-  return gulp.src('./client/js/app.js', {
-    read: false
-  })
-    .pipe(browserify({
-      insertGlobals: false,
-      transform: ['reactify'],
-      extensions: ['.jsx'],
-      debug: !gulp.env.production
-    }))
-    .pipe(gulpif(gulp.env.production, uglify({
-      mangle: {
-        except: ['require', 'export', '$super']
-      }
-    })))
-    .pipe(gulp.dest('./dist/js'));
+    return gulp.src('./client/js/app.js', {
+        read: false
+    })
+        .pipe(browserify({
+            insertGlobals: false,
+            transform: ['reactify'],
+            extensions: ['.jsx'],
+            debug: !gulp.env.production
+        }))
+        .pipe(gulpif(gulp.env.production, uglify({
+            mangle: {
+                except: ['require', 'export', '$super']
+            }
+        })))
+        .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('styles', function() {
-  return gulp.src('./client/scss/main.scss')
-    .pipe(sass({
-      outputStyle: gulp.env.production ? 'compressed' : 'expanded',
-      includePaths: ['./client/scss'].concat(bourbon),
-      errLogToConsole: gulp.env.watch
-    }))
-    .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./client/scss/main.scss')
+        .pipe(sass({
+            outputStyle: gulp.env.production ? 'compressed' : 'expanded',
+            includePaths: ['./client/scss'].concat(bourbon),
+            errLogToConsole: gulp.env.watch
+        }))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('html', function() {
-  return gulp.src('./client/**/*.html')
-    .pipe(gulp.dest('./dist'));
+    return gulp.src('./client/**/*.html')
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch-sass', function() {
-  return gulp.src('./client/scss/**/*.scss')
-    .pipe(watch())
-    .pipe(sass({
-      outputStyle: gulp.env.production ? 'compressed' : 'expanded',
-      includePaths: ['./client/scss'].concat(bourbon),
-      errLogToConsole: gulp.env.watch
-    }))
-    .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./client/scss/**/*.scss')
+        .pipe(watch())
+        .pipe(sass({
+            outputStyle: gulp.env.production ? 'compressed' : 'expanded',
+            includePaths: ['./client/scss'].concat(bourbon),
+            errLogToConsole: gulp.env.watch
+        }))
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('default', function() {
-  gulp.env.watch = true;
+    gutil.env.watch = true;
 
-  gulp.watch('./client/**/*.html', function(evt) {
-    gulp.run('html', function() {});
-  });
+    gulp.watch('./client/**/*.html', function(evt) {
+        gulp.run('html', function() {});
+    });
 
-  gulp.watch('./client/js/**', function(evt) {
-    gulp.run('scripts', function() {});
-  });
+    gulp.watch('./client/js/**', function(evt) {
+        gulp.run('scripts', function() {});
+    });
 
-  gulp.watch('client/scss/**', function(evt) {
-    gulp.run('styles', function() {});
-  });
+    gulp.watch('client/scss/**', function(evt) {
+        gulp.run('styles', function() {});
+    });
 });
 
 gulp.task('build', ['styles', 'scripts', 'html']);
